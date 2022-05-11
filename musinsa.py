8464abe3d1f2c7ef20ce6cf5ee1792dd6d98eb4a
kimsuchan1227 <greenpia1310@gmail.com>
2022-05-11 15:47:17 +0900

    text box

--- musinsa.py
+++ musinsa.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.messagebox as mbox
+import tkinter.simpledialog as sim
 
 def click(event) :
     mbox.showinfo("",(event.x,event.y))
@@ -11,6 +12,8 @@ window.geometry("500x500")
 window.resizable(width=False, height=False)
 
 window.bind("<Button-1>",click)
+
+sim.askinteger("","number",maxvalue=10)
 
 tk.Label(text="Hello", font=("",30), fg="orange").pack(side="left")
 
